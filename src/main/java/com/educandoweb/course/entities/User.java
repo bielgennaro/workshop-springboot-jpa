8f6cf7c6604a0465b426bc616efd3ff0d2e36512
Gabriel Zirondi Di Gennaro <zirondigabriel@gmail.com>
2024-03-15 01:48:55 -0300

    Adicionando dependencias H2 e Jpa

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.entities;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,17 +10,21 @@ import java.util.Objects;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
-    
+
     @Serial
     private static final long serialVersionUID = 1L;
-
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
-    
+
     public User() {
     }
 
