67e89fbe26d1a6dfc2449008ac03ed161bd112fc
bielgennaro <zirondigabriel@gmail.com>
2024-05-07 01:15:17 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -6,6 +6,8 @@ import lombok.Setter;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Getter
@@ -24,6 +26,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+    
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
