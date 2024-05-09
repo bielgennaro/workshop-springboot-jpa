434ff82a21c45da0d653f0d41919776dfddae30f
bielgennaro <zirondigabriel@gmail.com>
2024-05-09 02:31:45 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,11 +1,10 @@
 package com.educandoweb.course.config;
 
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
-import com.educandoweb.course.resources.OrderResource;
-import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -32,9 +31,9 @@ public class TestConfig implements CommandLineRunner {
         User u2 = new User(null, "Felipe Silva", "4399999", "felipe@gmail.com", "12345");
         User u3 = new User(null, "Lucas Andrade", "4399999", "lucas@gmail.com", "12345");
 
-        Order o1 = new Order(null, Instant.parse("2024-06-12T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2024-02-20T11:32:02Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2024-08-22T09:23:02Z"), u3);
+        Order o1 = new Order(null, Instant.parse("2024-06-12T19:53:07Z"), OrderStatus.DELIVERED, u1);
+        Order o2 = new Order(null, Instant.parse("2024-02-20T11:32:02Z"), OrderStatus.PAID, u2);
+        Order o3 = new Order(null, Instant.parse("2024-08-22T09:23:02Z"), OrderStatus.SHIPPED, u3);
         
         userRepository.saveAll(Arrays.asList(u1, u2, u3));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.entities;
 
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -26,15 +27,18 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
     
+    private Integer orderStatus;
+    
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
     
     public Order(){}
     
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        this.orderStatus = orderStatus.getCode();
         this.client = client;
     }
 
